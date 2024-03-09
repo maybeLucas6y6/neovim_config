549f1c276ffff32f7c0b152c31c3048b61a89c36
maybeLucas6y6 <petro.luca2004@gmail.com>
2024-03-10 00:59:35 +0200

    some init settings

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -28,6 +28,9 @@ opt.softtabstop = 4
 --opt.undodir = os.getenv("HOME") .. "/.undodir/"
 --opt.undofile = true
 
+-- virtual mode
+opt.virtualedit = "block"
+
 opt.fillchars = { eob = " " }
 opt.ignorecase = true
 opt.smartcase = true
@@ -61,7 +64,7 @@ g.mapleader = " "
 
 -- disable some default providers
 for _, provider in ipairs { "node", "perl", "python3", "ruby" } do
-  vim.g["loaded_" .. provider .. "_provider"] = 0
+    vim.g["loaded_" .. provider .. "_provider"] = 0
 end
 
 -- add binaries installed by mason.nvim to path
@@ -73,77 +76,77 @@ local autocmd = vim.api.nvim_create_autocmd
 
 -- dont list quickfix buffers
 autocmd("FileType", {
-  pattern = "qf",
-  callback = function()
-    vim.opt_local.buflisted = false
-  end,
+    pattern = "qf",
+    callback = function()
+        vim.opt_local.buflisted = false
+    end,
 })
 
 -- reload some chadrc options on-save
 autocmd("BufWritePost", {
-  pattern = vim.tbl_map(function(path)
-    return vim.fs.normalize(vim.loop.fs_realpath(path))
-  end, vim.fn.glob(vim.fn.stdpath "config" .. "/lua/custom/**/*.lua", true, true, true)),
-  group = vim.api.nvim_create_augroup("ReloadNvChad", {}),
+    pattern = vim.tbl_map(function(path)
+        return vim.fs.normalize(vim.loop.fs_realpath(path))
+    end, vim.fn.glob(vim.fn.stdpath "config" .. "/lua/custom/**/*.lua", true, true, true)),
+    group = vim.api.nvim_create_augroup("ReloadNvChad", {}),
 
-  callback = function(opts)
-    local fp = vim.fn.fnamemodify(vim.fs.normalize(vim.api.nvim_buf_get_name(opts.buf)), ":r") --[[@as string]]
-    local app_name = vim.env.NVIM_APPNAME and vim.env.NVIM_APPNAME or "nvim"
-    local module = string.gsub(fp, "^.*/" .. app_name .. "/lua/", ""):gsub("/", ".")
+    callback = function(opts)
+        local fp = vim.fn.fnamemodify(vim.fs.normalize(vim.api.nvim_buf_get_name(opts.buf)), ":r") --[[@as string]]
+        local app_name = vim.env.NVIM_APPNAME and vim.env.NVIM_APPNAME or "nvim"
+        local module = string.gsub(fp, "^.*/" .. app_name .. "/lua/", ""):gsub("/", ".")
 
-    require("plenary.reload").reload_module "base46"
-    require("plenary.reload").reload_module(module)
-    require("plenary.reload").reload_module "custom.chadrc"
+        require("plenary.reload").reload_module "base46"
+        require("plenary.reload").reload_module(module)
+        require("plenary.reload").reload_module "custom.chadrc"
 
-    config = require("core.utils").load_config()
+        config = require("core.utils").load_config()
 
-    vim.g.nvchad_theme = config.ui.theme
-    vim.g.transparency = config.ui.transparency
+        vim.g.nvchad_theme = config.ui.theme
+        vim.g.transparency = config.ui.transparency
 
-    -- statusline
-    require("plenary.reload").reload_module("nvchad.statusline." .. config.ui.statusline.theme)
-    vim.opt.statusline = "%!v:lua.require('nvchad.statusline." .. config.ui.statusline.theme .. "').run()"
+        -- statusline
+        require("plenary.reload").reload_module("nvchad.statusline." .. config.ui.statusline.theme)
+        vim.opt.statusline = "%!v:lua.require('nvchad.statusline." .. config.ui.statusline.theme .. "').run()"
 
-    -- tabufline
-    if config.ui.tabufline.enabled then
-      require("plenary.reload").reload_module "nvchad.tabufline.modules"
-      vim.opt.tabline = "%!v:lua.require('nvchad.tabufline.modules').run()"
-    end
+        -- tabufline
+        if config.ui.tabufline.enabled then
+            require("plenary.reload").reload_module "nvchad.tabufline.modules"
+            vim.opt.tabline = "%!v:lua.require('nvchad.tabufline.modules').run()"
+        end
 
-    require("base46").load_all_highlights()
-    -- vim.cmd("redraw!")
-  end,
+        require("base46").load_all_highlights()
+        -- vim.cmd("redraw!")
+    end,
 })
 
 -- user event that loads after UIEnter + only if file buf is there
 vim.api.nvim_create_autocmd({ "UIEnter", "BufReadPost", "BufNewFile" }, {
-  group = vim.api.nvim_create_augroup("NvFilePost", { clear = true }),
-  callback = function(args)
-    local file = vim.api.nvim_buf_get_name(args.buf)
-    local buftype = vim.api.nvim_buf_get_option(args.buf, "buftype")
+    group = vim.api.nvim_create_augroup("NvFilePost", { clear = true }),
+    callback = function(args)
+        local file = vim.api.nvim_buf_get_name(args.buf)
+        local buftype = vim.api.nvim_buf_get_option(args.buf, "buftype")
 
-    if not vim.g.ui_entered and args.event == "UIEnter" then
-      vim.g.ui_entered = true
-    end
-
-    if file ~= "" and buftype ~= "nofile" and vim.g.ui_entered then
-      vim.api.nvim_exec_autocmds("User", { pattern = "FilePost", modeline = false })
-      vim.api.nvim_del_augroup_by_name "NvFilePost"
-
-      vim.schedule(function()
-        vim.api.nvim_exec_autocmds("FileType", {})
-
-        if vim.g.editorconfig then
-          require("editorconfig").config(args.buf)
+        if not vim.g.ui_entered and args.event == "UIEnter" then
+            vim.g.ui_entered = true
         end
-      end, 0)
-    end
-  end,
+
+        if file ~= "" and buftype ~= "nofile" and vim.g.ui_entered then
+            vim.api.nvim_exec_autocmds("User", { pattern = "FilePost", modeline = false })
+            vim.api.nvim_del_augroup_by_name "NvFilePost"
+
+            vim.schedule(function()
+                vim.api.nvim_exec_autocmds("FileType", {})
+
+                if vim.g.editorconfig then
+                    require("editorconfig").config(args.buf)
+                end
+            end, 0)
+        end
+    end,
 })
 
 -------------------------------------- commands ------------------------------------------
 local new_cmd = vim.api.nvim_create_user_command
 
 new_cmd("NvChadUpdate", function()
-  require "nvchad.updater"()
+    require "nvchad.updater" ()
 end, {})
